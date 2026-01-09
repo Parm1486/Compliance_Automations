a64b13c704790adc956a93d508ff507ed98ed125
Parminder Singh <159745692+Parm1486@users.noreply.github.com>
2026-01-09 14:37:12 -0800

    Create front_running_detection.sql

---
+++ USA/front_running_detection.sql
@@ -0,0 +1,17 @@
+-- Front Running Detection 
+-- Goal: To identify trades occuring within 30 mins of a client trade. 
+-- Execution: Reconcile Personal_Trade and Client_Trade files to see if there is any trade occured in same ticker within 30 minutes.
+
+SELECT 
+    p.Trade_ID AS Personal_Trade,
+    c.Trade_ID AS Client_Trade,
+    p.Ticker,
+    p.Timestamp AS Personal_Time,
+    c.Timestamp AS Client_Time
+FROM Personal_Trades p
+JOIN Client_Trades c ON p.Ticker = c.Ticker
+WHERE p.Action = 'BUY' 
+  AND c.Action = 'BUY'
+  AND p.Timestamp < c.Timestamp
+  AND c.Timestamp <= (p.Timestamp + INTERVAL '30 minutes');
+     
