c880b8fe3ac5da23a2a0d1822cdb4ec00024949d
Parminder Singh <159745692+Parm1486@users.noreply.github.com>
2026-01-10 09:15:19 -0800

    Create trade_reconcillation.sql

---
+++ USA/trade_reconcillation.sql
@@ -0,0 +1,14 @@
+-- Trade Reconciliation 
+-- Identify discrepancies between Internal_Books and Broker_Records
+
+SELECT
+	i.Trade_ID,
+    i.Ticker,
+    i.Price AS Internal_Price,
+    b.Price AS Broker_Price,
+    (i.Price - b.Price) AS Price_Difference,
+    i.Quantity AS Internal_Qty,
+    b.Quantity AS Broker_Qty
+FROM Internal_Trades_i
+JOIN Broker_Statements b ON i.Trade_ID = b.Trade_ID
+WHERE i.Price <> b.Price OR i.Quantity <> b.Quantity;
