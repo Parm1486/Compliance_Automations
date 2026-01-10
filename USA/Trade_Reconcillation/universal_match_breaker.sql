a61799c7016290175cae1c87894ccdfbda2993b0
Parminder Singh <159745692+Parm1486@users.noreply.github.com>
2026-01-10 11:47:00 -0800

    Create universal_match_breaker.sql

---
+++ USA/Trade_Reconcillation/universal_match_breaker.sql
@@ -0,0 +1,15 @@
+-- Universal Match & Break Reporter
+-- To identify if a trade is 'Matched', 'Price_Break', or 'Quantity_Break'
+
+SELECT 
+    i.Trade_ID,
+    i.Asset_Class,
+    i.Ticker,
+    CASE 
+        WHEN i.Price = b.Price AND i.Quantity = b.Quantity THEN 'MATCHED'
+        WHEN i.Price <> b.Price AND i.Quantity = b.Quantity THEN 'PRICE_BREAK'
+        WHEN i.Price = b.Price AND i.Quantity <> b.Quantity THEN 'QTY_BREAK'
+        ELSE 'CRITICAL_MISMATCH'
+    END AS Reconciliation_Status
+FROM Internal_Trades i
+LEFT JOIN Broker_Statements b ON i.Trade_ID = b.Trade_ID;
