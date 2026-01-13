e1a14424eeb88f9c78e885314d4845b5165b1c11
Parminder Singh <159745692+Parm1486@users.noreply.github.com>
2026-01-13 06:25:38 -0800

    Create corporate_action_normalization.sql

---
+++ USA/Forensics/corporate_action_normalization.sql
@@ -0,0 +1,17 @@
+-- Corporate Action Normalization
+-- Focus: Match trades even if a stock split has occurred
+
+SELECT 
+    i.Ticker,
+    i.Quantity AS Internal_Qty,
+    -- Multiply internal qty by the split factor (e.g., 4.0)
+    (i.Quantity * ca.Split_Factor) AS Normalized_Internal_Qty,
+    b.Quantity AS Broker_Qty,
+    CASE 
+        WHEN (i.Quantity * ca.Split_Factor) = b.Quantity THEN 'MATCH_POST_SPLIT'
+        ELSE 'GENUINE_BREAK'
+    END AS Status
+FROM Internal_Positions i
+JOIN Broker_Positions b ON i.Ticker = b.Ticker
+JOIN Corp_Action_Calendar ca ON i.Ticker = ca.Ticker
+WHERE ca.Effective_Date = CURRENT_DATE;
